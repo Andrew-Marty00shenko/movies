58b1569aaf3a71630ee03366c0f419b429b94d98
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2020-06-07 00:32:54 +0300

    Change Raiting

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Rating from '@material-ui/lab/Rating';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 import Box from '@material-ui/core/Box';
 import { Button } from '@material-ui/core'
 
 export default function CustomizedRatings() {
-
     return (
         <div>
             <Box component="fieldset" mb={3} borderColor="transparent">
-                <Rating name="customized-10" defaultValue={2} max={10} /><br />
+                <Rating
+                    name="customized-empty"
+                    defaultValue={0}
+                    max={10}
+                    precision={0.5}
+                    emptyIcon={<StarBorderIcon fontSize="inherit" />}
+                /> <br />
                 <Button type="submit" variant="contained" color="primary" style={{ fontSize: "10px", height: "30px" }}>Отправить</Button>
             </Box>
-        </div >
+        </div>
     );
 }
-
